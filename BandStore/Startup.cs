6eafc4719759985df8ff31c6a4bf626f27b3fedf
puszynski <33966744+puszynski@users.noreply.github.com>
2018-04-25 23:09:19 +0200

    Add project files.

---
+++ BandStore/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BandStore.Startup))]
+namespace BandStore
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
