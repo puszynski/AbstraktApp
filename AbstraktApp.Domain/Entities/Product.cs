ae7e6fd4fe8b217274a054d8e3d663e63e37ce70
puszynski <33966744+puszynski@users.noreply.github.com>
2018-05-19 01:19:59 +0200

    Admin part One

--- AbstraktApp.Domain/Entities/Product.cs
+++ AbstraktApp.Domain/Entities/Product.cs
@@ -1,13 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
 using System.Text;
 
 namespace AbstraktApp.Domain.Entities
 {
     public class Product
     {
+        [HiddenInput(DisplayValue = false)]
         public int ProductID { get; set; }
         public string Name { get; set; }
+
+        [DataType(DataType.MultilineText)]
         public string Description { get; set; }
         public decimal Price { get; set; }
         public string Category { get; set; }
---
+++ BandStore/Controllers/AdminController.cs
@@ -0,0 +1,34 @@
+﻿using AbstraktApp.Domain.Abstract;
+using AbstraktApp.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BandStore.Controllers
+{
+    public class AdminController : Controller
+    {
+        private IProductRepository repository;
+
+        public AdminController(IProductRepository repository)
+        {
+            this.repository = repository;
+        }
+
+
+        // GET: Admin
+        public ActionResult Index()
+        {
+            return View(repository.Products);
+        }
+
+        public ActionResult Edit(int productID)
+        {
+            Product product = repository.Products.FirstOrDefault(p => p.ProductID == productID);
+
+            return View(product);
+        }
+    }
+}
